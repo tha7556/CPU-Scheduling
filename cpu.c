f72176436332c529fdd4d961f8b1f76d114cd8f9
Tyler <tha7556@users.noreply.github.com>
2017-10-16 16:18:37 -0400

    Added dispatch()

--- cpu.c
+++ cpu.c
@@ -25,10 +25,30 @@ void cpu_init()
 
 void dispatch()
 {
+    PCB *p = frontValue(&queue);
+    if(PTBR != NULL && PTBR->pcb->status == running) { //If a process is already running
+        PTBR->pcb->priority = PTBR-> pcb->burst_estimate - PTBR->pcb->this_cpuburst; //Priority = estimate - bursts so far
+
+        if(compareTo(PTBR->pcb,p) <= 0) {//PTBR <= head of queue priority
+            PTBR->pcb->last_dispatch = get_clock();
+            return;
+        }
+    }
+    else { //No process running
+        if(isEmpty(&queue)) { //queue is empty
+            PTBR = NULL;
+            return;
+        }
+    }
+    insert_ready(PTBR->pcb);
+    deQueue(&queue);
+    p->status = running;
+    PTBR = p->page_tbl;
+    prepage(p);
+    p->last_dispatch = get_clock();
+
 
 }
-
-
 
 void insert_ready(PCB *pcb)
 {
