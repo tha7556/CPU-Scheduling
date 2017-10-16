e88059e4c01385938cee6505f0945cbee2368550
d-dtm <dylan.menchetti@gmail.com>
2017-10-16 13:54:49 -0400

    cmpTo() and toString() methods

--- cpu.c
+++ cpu.c
@@ -16,6 +16,7 @@ char *toString(PCB *pcb);
 
 void cpu_init()
 {
+
 }
 
 
@@ -28,16 +29,21 @@ void dispatch()
 
 void insert_ready(PCB *pcb)
 {
+
 }
 
 
 char *toString(PCB *pcb)
 {
+    static char result[BUFSIZ];
+    sprintf (result, "pcb-%d(prior-%d) ", pcb->pcb_id, pcb->priority);
+    return result;
 }
 
 
 int compareTo(PCB *pcb1, PCB *pcb2)
 {
+    return pcb1 -> priority - pcb2 ->priority;
 }
 
 
