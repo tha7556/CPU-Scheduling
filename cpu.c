241b5eeaebe9c64fd2562f8430f8d86a68b9ca51
Tyler <tha7556@users.noreply.github.com>
2017-10-17 18:26:30 -0400

    DEBUGS

--- cpu.c
+++ cpu.c
@@ -14,7 +14,7 @@
 int compareTo(PCB *pcb1, PCB *pcb2);
 char *toString(PCB *pcb);
 
-Queue queue; //This is probably wrong
+Queue queue;
 void cpu_init()
 {
     set_timer(1000000);
@@ -25,42 +25,59 @@ void cpu_init()
 
 void dispatch()
 {
+	if (______trace_switch) { printf("dispatch called\n"); }
     PCB *p = frontValue(&queue);
-    if(PTBR != NULL && PTBR->pcb->status == running) { //If a process is already running
+    if(PTBR != NULL && PTBR->pcb != NULL && PTBR->pcb->status == running) { //If a process is already running
         PTBR->pcb->priority = PTBR-> pcb->burst_estimate - PTBR->pcb->this_cpuburst; //Priority = estimate - bursts so far
-
+        if (______trace_switch) { printf("1\n"); }
         if(compareTo(PTBR->pcb,p) <= 0) {//PTBR <= head of queue priority
+            if (______trace_switch) { printf("2\n"); }
             PTBR->pcb->last_dispatch = get_clock();
             return;
         }
     }
     else { //No process running
+        if (______trace_switch) { printf("3\n"); }
         if(isEmpty(&queue)) { //queue is empty
+                if (______trace_switch) { printf("4\n"); }
             PTBR = NULL;
             return;
         }
     }
-    insert_ready(PTBR->pcb);
+    if (______trace_switch) { printf("5\n"); }
+    if(PTBR != NULL)
+        insert_ready(PTBR->pcb);
     deQueue(&queue);
     p->status = running;
     PTBR = p->page_tbl;
+    if(______trace_switch) {printf("starting prepaging\n");}
     prepage(p);
     p->last_dispatch = get_clock();
+	if (______trace_switch) {
+                    printf("dispatch finished\n");
+                }
 
 
 }
 
 void insert_ready(PCB *pcb)
 {
-
+    int alpha = .5;
+    int initial = 10;
+	if (______trace_switch) {
+                    printf("insertReady called\n");
+                }
     if (pcb -> last_dispatch == 0) //If it is a new process
-        pcb -> burst_estimate = 5;
+        pcb -> burst_estimate = initial;
     else //If the process has been run before
-        pcb -> burst_estimate = (pcb -> burst_estimate*.6) + (.4*pcb-> last_cpuburst);
+        pcb -> burst_estimate = (pcb -> burst_estimate*alpha) + ((1-alpha)*pcb-> last_cpuburst);
 
     pcb -> priority = pcb -> burst_estimate; //Priority = burst estimate
     pcb -> status = ready;
     enQueueSorted(&queue, pcb, *compareTo ); //Add to priority queue
+	if (______trace_switch) {
+                    printf("insertReady finished\n");
+                }
 
 }
 
